168991b71b2f6928853eda27c1eb06e4bcf3ab05
subratshakya <subratshakya20@gmail.com>
2025-04-16 11:54:06 +0530

    Ecommerce completed

--- models/ecommerce/order.models.js
+++ models/ecommerce/order.models.js
@@ -1,5 +1,16 @@
 import mongoose from "mongoose"
 
+const OrderItemSchema=new mongoose.Schema({
+    productId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Product"
+    },
+    quantity:{
+        type:Number,
+        required:true
+    }
+})
+
 const orderSchema=new mongoose.Schema({
     orderPrice:{
         type:Number,
@@ -9,7 +20,18 @@ const orderSchema=new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:"User"
     },
-    
+    OrderItems:{ 
+        type:[OrderItemSchema]
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["PENDING","CANCELLED","DELIVERED"],
+        default:"PENDING"
+    }
 },{timestamps:true}
 )
 
