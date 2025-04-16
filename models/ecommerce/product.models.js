9501f94b275ab4c24683282dc5406902ba5240fa
subratshakya <subratshakya20@gmail.com>
2025-04-16 11:46:42 +0530

    ecomm changes

--- models/ecommerce/product.models.js
+++ models/ecommerce/product.models.js
@@ -1,1 +1,39 @@
 import mongoose from "mongoose";
+
+const productSchema=new mongoose.Schema(
+    {
+        description:{
+            required:true,
+            type: String
+        },
+        name:{
+            required:true,
+            type: String 
+        },
+        productImage:{
+            type:String
+        },
+        price:{
+            type:Number,
+            default:0
+        },
+        stock:{
+            type:Number,
+            default:0
+        },
+        Category:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Category",
+            required:true,
+        },
+        owner:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+        },
+
+
+    }, {timestamps:true}
+)
+
+
+export const Product=mongoose.modell("Product",productSchema)
